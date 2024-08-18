5a0fc6f0fd144f52bbf53706e7bfbcdb362c64ad
Haniebonjakhi <haniebonjakhi@gmail.com>
2024-08-18 11:56:47 +0430

    heb3.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -46,25 +46,8 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
-//UART_HandleTypeDef huart2;  
-//DMA_HandleTypeDef hdma_uart2_tx; 
-
-//uint8_t txBuffer[] = "Mehbang";
-//uint16_t txBufferSize = sizeof(txBuffer) - 1;
-//uint8_t txIndex =0 ;
-
 uint8_t data1[] = "Mehbang";
 uint16_t sizeofdata = 7;
-
-uint8_t a = 0;
-uint8_t b = 0;
-uint8_t c = 0;
-uint8_t d = 0;
-uint8_t e = 0;
-uint8_t f = 0;
-uint8_t h = 0;
-
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -75,55 +58,21 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
-// DMA
-//void DMA1_Channel7_Init(void) {
-//    RCC->AHBENR |= RCC_AHBENR_DMA1EN;
-
-//    DMA1_Channel7->CCR = 0;
-//    while (DMA1_Channel7->CCR & DMA_CCR_EN);
-
-//    DMA1_Channel7->CPAR = (uint32_t)&USART2->DR;    
-//    DMA1_Channel7->CMAR = (uint32_t)txBuffer;
-//    DMA1_Channel7->CNDTR = txBufferSize;              
-
-//	
-//    DMA1_Channel7->CCR = DMA_CCR_MINC |      
-//                         DMA_CCR_DIR |            
-//                         DMA_CCR_TCIE |         
-//                         DMA_CCR_PL_1;            
-//}
-
-//void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-
-//	DMA1_Channel7->CCR &= ~DMA_CCR_EN; 
-//  DMA1_Channel7->CNDTR = txBufferSize;
-//  DMA1_Channel7->CCR |= DMA_CCR_EN;
-//	
-//}
-
-
-
 void USART2_DMA_Init()  
 {  
-		a=1;
 		RCC->AHBENR |= RCC_AHBENR_DMA1EN;
 		DMA1_Channel7->CCR = 0;
 		USART2->CR3 |= USART_CR3_DMAT; 
 	
-    // Ensure that DMA Channel 7 is ready for the transmission  
-    while (DMA1_Channel7->CCR & DMA_CCR_EN); // Wait until the current DMA transfer (if any) is complete  
-		b=1;
+    while (DMA1_Channel7->CCR & DMA_CCR_EN); 
 	
-    // Set peripheral and memory addresses  
-    DMA1_Channel7->CPAR = (uint32_t)&USART2->DR;  // Peripheral address (USART TDR register)  
-    DMA1_Channel7->CMAR = (uint32_t)data1;            // Memory address (Data buffer)  
-    DMA1_Channel7->CNDTR = sizeofdata;                     // Number of data items to transfer  
+    DMA1_Channel7->CPAR = (uint32_t)&USART2->DR;   
+    DMA1_Channel7->CMAR = (uint32_t)data1;            
+    DMA1_Channel7->CNDTR = sizeofdata;                 
 
-    // Configure the DMA transfer  
-    DMA1_Channel7->CCR |= DMA_CCR_DIR;               // Set data direction (Memory to Peripheral)  
-    DMA1_Channel7->CCR |= DMA_CCR_MINC;              // Enable memory address increment  
-    DMA1_Channel7->CCR |= DMA_CCR_TCIE;              // Enable transfer complete interrupt  
+    DMA1_Channel7->CCR |= DMA_CCR_DIR;               
+    DMA1_Channel7->CCR |= DMA_CCR_MINC;              
+    DMA1_Channel7->CCR |= DMA_CCR_TCIE;            
 		DMA1_Channel7->CCR |= DMA_CCR_PL_1;
 
 } 
@@ -131,56 +80,10 @@ void USART2_DMA_Init()
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-	c=1;
 	DMA1_Channel7->CCR &= ~DMA_CCR_EN;
 	DMA1_Channel7->CNDTR = sizeofdata;                     
 	DMA1_Channel7->CCR |= DMA_CCR_EN;
 }
-
-
-//void usart_config(void)  
-//{  
-//		e=1;
-//    // 1. Enable the USART by writing the UE bit in USART_CR1 register to 1.  
-//    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;   
-
-
-//    // 2. Configure USART2 settings   
-//    huart2.Instance = USART2;  
-//    huart2.Init.BaudRate = 115200;         
-//    huart2.Init.WordLength = UART_WORDLENGTH_8B;  
-//    huart2.Init.StopBits = UART_STOPBITS_1;  
-//    huart2.Init.Parity = UART_PARITY_NONE;  
-//    huart2.Init.Mode = UART_MODE_TX;     
-//    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;  
-//    huart2.Init.OverSampling = UART_OVERSAMPLING_16;  
-//    
-//    HAL_UART_Init(&huart2);  
-
-//    USART2->CR1 |= USART_CR1_UE; 
-//}  
-
-//void DMA_Init(void)  
-//{  
-//			f=1;
-//	
-//    // Enable DMA1 clock  
-//    __HAL_RCC_DMA1_CLK_ENABLE();  
-
-//    // Configure DMA for USART2 TX  
-//    hdma_uart2_tx.Instance = DMA1_Channel7;  // Use DMA1 Channel 7 for TX  
-//    hdma_uart2_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;  
-//    hdma_uart2_tx.Init.PeriphInc = DMA_PINC_DISABLE;  
-//    hdma_uart2_tx.Init.MemInc = DMA_MINC_ENABLE;  
-//    hdma_uart2_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;  
-//    hdma_uart2_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;  
-//    hdma_uart2_tx.Init.Mode = DMA_NORMAL;  
-//    hdma_uart2_tx.Init.Priority = DMA_PRIORITY_LOW;  
-//    HAL_DMA_Init(&hdma_uart2_tx);  
-
-//    // Link DMA TX to USART2  
-//    __HAL_LINKDMA(&huart2, hdmatx, hdma_uart2_tx);  
-//}  
 
 /* USER CODE END 0 */
 
@@ -218,8 +121,6 @@ int main(void)
   /* USER CODE BEGIN 2 */
 	USART2_DMA_Init();
 	HAL_TIM_Base_Start_IT(&htim2);
-	//	DMA_Init();
-	//	usart_config();
   /* USER CODE END 2 */
 
   /* Infinite loop */
